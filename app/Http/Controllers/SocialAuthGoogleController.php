a6e44c1fc8ce79fddd01b0b447e463b5c8bd2e44
Farouk00 <44416222+Farouk00@users.noreply.github.com>
2020-05-11 02:55:39 +0200

    login files re-uploaded

---
+++ app/Http/Controllers/SocialAuthGoogleController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; 
+  use App\User;
+  use Socialite;
+  use Auth; 
+  use Exception;
+
+class SocialAuthGoogleController extends Controller
+{
+    //
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+
+    public function callback()
+    {
+        try {
+            
+        
+            $googleUser = Socialite::driver('google')->user();
+            $existUser = User::where('email',$googleUser->email)->first();
+            
+
+            if($existUser) {
+                Auth::loginUsingId($existUser->id);
+            }
+            else {
+                $user = new User;
+                $user->name = $googleUser->name;
+                $user->email = $googleUser->email;
+                $user->google_id = $googleUser->id;
+                $user->password = md5(rand(1,10000));
+                $user->save();
+                Auth::loginUsingId($user->id);
+            }
+            return redirect()->to('/home');
+        } 
+        catch (Exception $e) {
+            return 'error';
+        }
+    }
+}
